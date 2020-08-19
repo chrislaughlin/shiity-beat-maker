205b8056af7ce1557cb0162647aef9eb58b75f0a
Chris Laughlin <chris_laughlin@rapid7.com>
2020-08-19 19:05:41 +0100

    changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,72 @@
 import React, {useState} from 'react';
-import DEFAULT_BEATS from "./defaultBeats";
+import {
+    DEFAULT_BEATS,
+    CLEAR_BEATS
+} from "./defaultBeats";
 import Beats from "./beats";
 import CurrentPlaying from "./currentPlaying";
+
+const clapSound = new Audio('clap.wav')
+const hatSound = new Audio('hat.wav')
+const kickSound = new Audio('kick.wav')
+
+const playClap = () => {
+    setTimeout(() => {
+        clapSound.play();
+    }, 200);
+    setTimeout(() => {
+        hatSound.play();
+    }, 400);
+    setTimeout(() => {
+        kickSound.play();
+    }, 600);
+}
+
+const beatMapping = [clapSound, hatSound, kickSound];
+
+
+const playThatFunkyMusic = (beatList, setCurrentPlaying) => {
+    let currentIndex = 0;
+    const beatInterval = setInterval(() => {
+        const line = beatList[currentIndex];
+        console.log(line.join(' '));
+
+        let beatTimeout = 2;
+
+        line.forEach((beat, index) => {
+            if (beat === 'X') {
+                setTimeout(() => {
+                    setCurrentPlaying(line);
+                    beatMapping[index].play();
+                }, 100 * beatTimeout);
+                beatTimeout = beatTimeout + 2;
+            }
+        })
+
+        // if (line[0] === 'X') {
+        //     setTimeout(() => {
+        //         clapSound.play();
+        //     }, 100);
+        // }
+        //
+        // if (line[1] === 'X') {
+        //     setTimeout(() => {
+        //         hatSound.play();
+        //     }, 200);
+        // }
+        //
+        // if (line[2] === 'X') {
+        //     setTimeout(() => {
+        //         kickSound.play();
+        //     }, 300);
+        // }
+
+        currentIndex++;
+        if (currentIndex === beatList.length) {
+            clearInterval(beatInterval);
+        }
+    }, 500);
+};
 
 function App() {
     const [beats, setBeats] = useState(DEFAULT_BEATS);
@@ -9,8 +74,7 @@ function App() {
     const [currentlyPlaying, setCurrentlyPlaying] = useState({
         0: false,
         1: false,
-        2: false,
-        3: false,
+        2: false
     })
 
     const changeBeat = (l, b, currValue) => {
@@ -40,8 +104,33 @@ function App() {
                     beats={beats}
                     changeBeat={changeBeat}
                 />
-                <button className="play-button">
+                <button
+                    className="play-button"
+                    onClick={() => {
+                        playThatFunkyMusic(
+                            beats,
+                            currPlaying => {
+                                currPlaying.forEach((b, index) => {
+                                    setCurrentlyPlaying(curr => {
+                                        return {
+                                            ...curr,
+                                            [index]: b === 'X'
+                                        };
+                                    });
+                                })
+                            }
+                        )
+                    }}
+                >
                     {playing ? 'STOP' : 'PLAY'}
+                </button>
+                <button
+                    className="play-button"
+                    onClick={() => setBeats(
+                        CLEAR_BEATS
+                    )}
+                >
+                    CLEAR
                 </button>
             </section>
         </div>
--- src/beats.js
+++ src/beats.js
@@ -2,34 +2,36 @@ import React from 'react';
 
 const Beats = ({ beats, changeBeat }) => {
     return (
-        <div className="beats">
-            {
-                beats.map((line, lineIndex) => {
-                    return (
-                        <div
-                            key={lineIndex}
-                            className="line"
-                        >
-                            {
-                                line.map((beat, beatIndex) => {
-                                    return (
-                                        <div
-                                            key={`${lineIndex}-${beatIndex}`}
-                                            className="beat"
-                                            onClick={() => {
-                                                changeBeat(lineIndex, beatIndex, beat);
-                                            }}
-                                        >
-                                            {beat}
-                                        </div>
-                                    )
-                                })
-                            }
-                        </div>
-                    )
-                })
-            }
-        </div>
+        <>
+            <div className="beats">
+                {
+                    beats.map((line, lineIndex) => {
+                        return (
+                            <div
+                                key={lineIndex}
+                                className="line"
+                            >
+                                {
+                                    line.map((beat, beatIndex) => {
+                                        return (
+                                            <div
+                                                key={`${lineIndex}-${beatIndex}`}
+                                                className="beat"
+                                                onClick={() => {
+                                                    changeBeat(lineIndex, beatIndex, beat);
+                                                }}
+                                            >
+                                                {beat}
+                                            </div>
+                                        )
+                                    })
+                                }
+                            </div>
+                        )
+                    })
+                }
+            </div>
+        </>
     );
 };
 
